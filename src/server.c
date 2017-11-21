7c79d439f0ac0acb073dcb1cb176a83b8ded822b
Tom Fleming <tom.fleming@adlinktech.com>
2017-11-21 15:26:12 +0000

    Added Pressure and Humidity nodes

--- src/server.c
+++ src/server.c
@@ -22,6 +22,31 @@ static void addNodes(UA_Server *server)
    const UA_NodeId myTempNodeId = UA_NODEID_STRING(1, "Temperature");
    UA_Server_addVariableNode(server, myTempNodeId, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), 
    UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), myTempName, UA_NODEID_NULL, myVar, NULL, NULL);
+   
+   myVar = UA_VariableAttributes_default;
+   myVar.description = UA_LOCALIZEDTEXT("en-US", "The pressure reading from the Pi Sense Hat");
+   myVar.displayName = UA_LOCALIZEDTEXT("en-US", "Pressure");
+   myVar.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+   myVar.dataType = UA_TYPES[UA_TYPES_FLOAT].typeId;
+   UA_Float myPress = 0.5;
+   UA_Variant_setScalarCopy(&myVar.value, &myPress, &UA_TYPES[UA_TYPES_FLOAT]);
+   const UA_QualifiedName myPressName = UA_QUALIFIEDNAME(1, "Pressure");
+   const UA_NodeId myPressNodeId = UA_NODEID_STRING(1, "Pressure");
+   UA_Server_addVariableNode(server, myPressNodeId, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), 
+   UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), myPressName, UA_NODEID_NULL, myVar, NULL, NULL);
+   UA_Variant_deleteMembers(&myVar.value);
+   
+   myVar = UA_VariableAttributes_default;
+   myVar.description = UA_LOCALIZEDTEXT("en-US", "The humidity reading from the Pi Sense Hat");
+   myVar.displayName = UA_LOCALIZEDTEXT("en-US", "Humidity");
+   myVar.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+   myVar.dataType = UA_TYPES[UA_TYPES_FLOAT].typeId;
+   UA_Float myHumid = 0.5;
+   UA_Variant_setScalarCopy(&myVar.value, &myHumid, &UA_TYPES[UA_TYPES_FLOAT]);
+   const UA_QualifiedName myHumidName = UA_QUALIFIEDNAME(1, "Humidity");
+   const UA_NodeId myHumidNodeId = UA_NODEID_STRING(1, "Humidity");
+   UA_Server_addVariableNode(server, myHumidNodeId, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER), 
+   UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), myHumidName, UA_NODEID_NULL, myVar, NULL, NULL);
    UA_Variant_deleteMembers(&myVar.value);
 }
 
@@ -42,33 +67,46 @@ void* pollSensors(void *arg){
    imu->setCompassEnable(true);
 
    //  set up pressure sensor
-
    if (pressure != NULL)
       pressure->pressureInit();
-
    //  set up humidity sensor
-
    if (humidity != NULL)
        humidity->humidityInit();
+       
    while (true)
    {
-  	//Create 'temperature' value
-   RTIMU_DATA imuData = imu->getIMUData();
-   if (pressure != NULL)
-       pressure->pressureRead(imuData);
+     	//Create 'temperature' value
+      RTIMU_DATA imuData = imu->getIMUData();
+      if (pressure != NULL)
+      {
+         pressure->pressureRead(imuData);
+      }
+      //  add the humidity data to the structur
+      if (humidity != NULL)
+      {
+         humidity->humidityRead(imuData);
+      }
 
-            //  add the humidity data to the structur
-  if (humidity != NULL)
-      humidity->humidityRead(imuData);
+      //Write Temperature
+      UA_Variant *myVariant = UA_Variant_new();
+      UA_Float tempIn = imuData.temperature;
+      UA_Variant_setScalarCopy(myVariant, &tempIn, &UA_TYPES[UA_TYPES_FLOAT]);
+      UA_NodeId tempNodeId = UA_NODEID_STRING(1,"Temperature");
+      UA_StatusCode status = UA_Client_writeValueAttribute(client,testNodeId, myVariant);
+      
+      //Write Pressure
+      UA_Float pressIn = imuData.pressure;
+      UA_Variant_setScalarCopy(myVariant, &pressIn, &UA_TYPES[UA_TYPES_FLOAT]);
+      UA_NodeId pressNodeId = UA_NODEID_STRING(1,"Pressure");
+      UA_StatusCode status = UA_Client_writeValueAttribute(client,pressNodeId, myVariant);
+      
+      //Write Humidity
+      UA_Float humidIn = imuData.humidity;
+      UA_Variant_setScalarCopy(myVariant, &humidIn, &UA_TYPES[UA_TYPES_FLOAT]);
+      UA_NodeId humidNodeId = UA_NODEID_STRING(1,"Humidity");
+      UA_StatusCode status = UA_Client_writeValueAttribute(client,humidNodeId, myVariant);
 
-   printf("Temp %4.1f\n",imuData.temperature);
-   UA_Variant *myVariant = UA_Variant_new();
-   UA_Float tempIn = imuData.temperature;
-   UA_Variant_setScalarCopy(myVariant, &tempIn, &UA_TYPES[UA_TYPES_FLOAT]);
-   UA_NodeId testNodeId = UA_NODEID_STRING(1,"Temperature");
-   UA_StatusCode status = UA_Client_writeValueAttribute(client,testNodeId, myVariant);
-   //Sleep for 1 second
-   sleep(imu->IMUGetPollInterval());
+      sleep(imu->IMUGetPollInterval());
    }
 }
 UA_Boolean running = true;
